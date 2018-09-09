801c746237b988f22b81c8830c178d5cf0e2d9e8
Timothy Dillman <liltimtim@gmail.com>
2018-09-09 08:48:24 -0400

    fix issue with other browsers

--- client/src/store.js
+++ client/src/store.js
@@ -3,14 +3,19 @@ import thunk from "redux-thunk";
 import reducers from "./reducers";
 const middleware = [thunk];
 
-const store = createStore(
-  reducers,
-  {},
-  compose(
-    applyMiddleware(...middleware),
-    // Implements the Chrome redux tools extension
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const whichCompose = () => {
+  if (window.navigator.userAgent.includes("Chrome")) {
+    compose(
+      applyMiddleware(...middleware),
+      // Implements the Chrome redux tools extension
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+  } else {
+    compose(applyMiddleware(...middleware));
+  }
+};
+
+const store = createStore(reducers, {}, whichCompose());
 
 export default store;
