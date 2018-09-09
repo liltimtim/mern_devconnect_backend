742f53c2108b9f901f100955b62c57e0e238b090
Timothy Dillman <liltimtim@gmail.com>
2018-09-08 21:09:39 -0400

    state based login and registration form before redux

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  /**
+   * Event triggered whenever any form input value changes
+   * @param {React.SyntheticEvent} e
+   */
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  /**
+   * Called when the form is submitted
+   * @param {React.SyntheticEvent} e
+   */
+  onSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">Create your DevConnect Account</p>
+              <form noValidate onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="First and Last Name"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
